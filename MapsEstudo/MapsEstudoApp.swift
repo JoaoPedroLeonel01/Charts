ace9fd438f373764881cd7bc4b1ffe9bf03fbd1a
Joao Pedro <joaopedreleonel@gmail.com>
2025-10-10 17:25:52 -0300

    Initial Commit

---
+++ MapsEstudo/MapsEstudoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsEstudoApp.swift
+//  MapsEstudo
+//
+//  Created by Joao pedro Leonel on 10/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MapsEstudoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
