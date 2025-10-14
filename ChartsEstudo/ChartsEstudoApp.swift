7fc238f351bcfde831f1e8f67b7b53bf84d7787d
Joao Pedro <joaopedreleonel@gmail.com>
2025-10-14 14:59:21 -0300

    Initial Commit

---
+++ ChartsEstudo/ChartsEstudoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsEstudoApp.swift
+//  ChartsEstudo
+//
+//  Created by Joao pedro Leonel on 14/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ChartsEstudoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
